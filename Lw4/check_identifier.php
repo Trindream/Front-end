ee4c97f9e44d8d644fb7541e400be9400dc9fc14
Ivanov Kirill <trindream@gmail.com>
2016-11-16 13:25:05 +0300

    Разделил логику и отображение.

--- Lw4/check_identifier.php
+++ Lw4/check_identifier.php
@@ -1,33 +1,48 @@
 <?
+    $ERR1 = 1;
+	$ERR2 = 2;
+	
     function checkIdentifier($arrOfChar)
 	{
-		$errors = "";
+		$error = 0;
 
 		if (!ctype_alpha($arrOfChar[0]))
 		{
-			$errors .= "Идентификатор должен начинаться с буквы<br>";
+			$error += $ERR1;
 		}
 		
 		for ($i = 1; $i < count($arrOfChar); $i++)
 		{
 			if (!is_numeric($arrOfChar[$i]) && !ctype_alpha($arrOfChar[$i]))
 			{
-				$errors .= "Идентификатор может состоять только из цифр и букв";
+				$error += $ERR2;
 				break;
 			}
 		}
-		return $errors;
+		return $error;
 	}
 	
-	function showResult($errors)
+	function showResult($error)
 	{
-		if (empty($errors))
+		if (!$error)
 		{
 			print("Yes");
 		}
 		else
 		{
-			print("No<br>" . $errors);
+			print("No<br>");
+			if ($error == $ERR1)
+			{
+				print("Идентификатор должен начинаться с буквы.");
+			}
+			else if ($error == $ERR2)
+			{
+				print("Идентификатор может состоять только из цифр и букв.");
+			}
+			else
+			{
+				print("Идентификатор должен начинаться с буквы, и состоять из цифр и букв.");
+			}
 		}
 	}
 	
